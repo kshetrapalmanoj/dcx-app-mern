6161458df2e68e3d9d4d0f98395e95c14f79e8f7
Manoj <kshetrapalmanoj@gmail.com>
2021-04-30 22:01:32 +0530

    added logout

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,28 @@
 // import './css/style.css';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import { Redirect } from 'react-router';
 import About from './About'
 import Dashboard from './Dashboard';
 import Register from './Register';
 import Browse from './Browse';
 import Contact from './Contact';
+import Login from './Login'
+
 function Home() {
+
+  const LoginGuardRoute = ({ component: Component, ...props }) => (
+    <Route
+      {...props}
+      render={routeProps => {
+        return localStorage.getItem("login") ?
+          (
+            <Component {...routeProps} />
+          ) : (
+            <Redirect to="/login" />
+          );
+      }}
+    />
+  );
 
   return (
     <Router>
@@ -23,21 +40,24 @@ function Home() {
               <li className="menuitem"><Link to="/browse">Browse Developers</Link></li>
               <li className="menuitem"><Link to="/register">Register a New Developer</Link></li>
               <li className="menuitem"><Link to="/contact">Contact Us</Link></li>
+              <li className="menuitem"><Link to="/login" onClick={() => localStorage.clear()} >Logout</Link></li>
             </ul>
           </nav>
-
-
         </div>
+
         <Switch>
-          <Route exact path="/" component={Dashboard}></Route>
-          <Route path="/dashboard" component={Dashboard}></Route>
-          <Route path="/about" component={About}></Route>
-          <Route path="/register" component={Register}></Route>
-          <Route path="/browse" component={Browse}></Route>
-          <Route path="/contact" component={Contact}></Route>
+
+          <LoginGuardRoute path="/dashboard" component={Dashboard} />
+          <LoginGuardRoute path="/register" component={Register} />
+          <LoginGuardRoute path="/browse" component={Browse} />
+          <Route path="/login" component={Login}></Route>
+          <LoginGuardRoute path="/about" component={About} />
+          <LoginGuardRoute path="/contact" component={Contact} />
+          <LoginGuardRoute exact path="/" component={Dashboard} />
+
         </Switch>
       </div>
-    </Router>
+    </Router >
 
   );
 }
---
+++ src/Components/Login.js
@@ -0,0 +1,135 @@
+import React, { Component } from 'react';
+import axios from "axios";
+import { environment } from '../services/DeveloperService';
+
+class Login extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      email: '',
+      password: '',
+      passwordError: "",
+      emailError: "",
+    }
+
+    this.emailHandler = this.emailHandler.bind(this)
+    this.passwordHandler = this.passwordHandler.bind(this)
+    this.login = this.login.bind(this)
+  }
+
+  validate = () => {
+    let passwordError = "";
+    let emailError = "";
+
+
+    if (!this.state.password) {
+      passwordError = "Password cannot be empty";
+    } else if (!this.state.password.match("(?=.*[@#$]).{8,}")) {
+      passwordError = "Password is invalid";
+    }
+    if (!this.state.email) {
+      emailError = "Email cannot be empty";
+    } else if (
+      !this.state.email.match("\\S+?@\\S+?\\.com")
+    ) {
+      emailError = "Email is invalid";
+    }
+
+    if (
+
+      passwordError ||
+      emailError
+
+    ) {
+      this.setState({
+        passwordError,
+        emailError,
+      });
+      return false;
+    }
+    return true;
+  };
+
+  emailHandler = (event) => {
+    this.setState({ email: event.target.value })
+  }
+
+  passwordHandler = (event) => {
+    this.setState({ password: event.target.value })
+  }
+
+  login = (event) => {
+    event.preventDefault();
+    const data = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+
+    const isValid = this.validate();
+    if (isValid) {
+      axios
+        .post(`${environment.baseUrl}/login`, data)
+        .then(
+          res => {
+            console.log(res)
+            localStorage.setItem('login', JSON.stringify({
+              login: true,
+              token: res.data
+            }))
+            this.props.history.push('/')
+          }
+        )
+        .catch(
+          err => {
+            console.log('Invalid credentials', err)
+            localStorage.removeItem('login')
+          }
+        );
+      console.log('Developer Details:', this.state);
+    }
+  }
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <h3 className="text-center mb-md-4">Login to Continue</h3>
+          <div className="row">
+            <div className="card col-md-6 offset-md-3 offset-md-3">
+              <div className="card-body">
+                <form>
+                  <div className="form-group">
+                    <label htmlFor="empEmail" className="font-weight-bold">Email Address</label>
+                    <input type="text" placeholder="eg. john.doe@gmail.com" id="empEmail" name="empEmail"
+                      className="form-control" value={this.state.email} onChange={this.emailHandler} />
+                  </div>
+                  <div className="text-danger">{this.state.emailError}</div>
+                  <br></br>
+
+                  <div className="form-group">
+                    <label htmlFor="empPassword" className="font-weight-bold">Password</label>
+                    <input type="password" placeholder="Choose a secure password" id="empPassword" name="empPassword"
+                      className="form-control" value={this.state.password} onChange={this.passwordHandler} />
+                  </div>
+                  <div className="text-danger">{this.state.passwordError}</div>
+                  <br></br>
+
+                  <div className="custom-control custom-checkbox">
+                    <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                    <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                  </div>
+                  <br></br>
+                  <div className="text-center">
+                    <button className="btn btn-success" onClick={this.login}>Login</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
